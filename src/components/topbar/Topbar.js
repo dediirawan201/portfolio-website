90e487199fbb17a952942fc29c15a71911963c48
dedi <dediirwansyah1925@gmail.com>
2021-08-31 17:17:15 +0700

    menggunakan material-ui icons untuk nav direact

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './topbar.scss'
+import {Person, Mail} from '@material-ui/icons'
 const Topbar = () => {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Dedi Irawan.</a>
+                    <div className='itemContainer'>
+                        <Person className="icon"/>
+                        <span>0896-14232-232</span>
+                    </div>
+                    <div className='itemContainer'>
+                        <Mail className="icon"/>
+                        <span>dedi@gmail.com</span>
+                    </div>
                 </div>
                 <div className="right">
                     hallo
